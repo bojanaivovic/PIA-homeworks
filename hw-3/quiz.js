7f4463876542a7772fcca2a89aa91d7681344a71
bojanaivovic <bbojana.ivovic@gmail.com>
2020-12-22 18:09:50 +0100

    Dodat alert i funkcionalnost za dugme nazad

--- hw-3/quiz.js
+++ hw-3/quiz.js
@@ -1,10 +1,24 @@
 function myFunction(){
     var input_name=document.forms["form"]["input_name"].value;
     if(input_name==""){
+        $('.alert').removeClass("hide");
+        $('.alert').addClass("show");
         return false;
     }
     else{
-        document.getElementById("podaci_korisnika").style.display="none";
-        document.getElementById("pravila_kviza").style.display="block";
+        $('.alert').removeClass("show");
+        $('.alert').addClass("hide");
+        $('#podaci_korisnika').hide();
+        $('#pravila_kviza').fadeIn();
     }
+}
+
+$('.close-btn').click(function(){
+    $('.alert').removeClass("show");
+    $('.alert').addClass("hide");
+})
+
+function nazad(){
+    $('#podaci_korisnika').fadeIn();
+     $('#pravila_kviza').hide();
 }
