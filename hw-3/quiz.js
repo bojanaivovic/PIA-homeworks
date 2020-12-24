6f7bf55e0eefcd342157ea3c7ab01689d27893f2
bojanaivovic <bbojana.ivovic@gmail.com>
2020-12-24 02:52:03 +0100

    dodate boje za tačnost odgovora

--- hw-3/quiz.js
+++ hw-3/quiz.js
@@ -33,57 +33,319 @@ function start(){
         $("#odg2").html(data.odgovori_pitanja1[1]);
         $("#odg3").html(data.odgovori_pitanja1[2]);
         $("#odg4").html(data.odgovori_pitanja1[3]);
-        setTimeout(next2, 3000);
+
+            $("#odg1").click(function(){
+                $("#odg1").css('background-color', '#a4161a');
+                $("#odg2").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next2, 3000);
+            })
+            
+            $("#odg2").click(function(){
+                $("#odg2").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next2, 3000);
+            })
+
+            $("#odg3").click(function(){
+                $("#odg3").css('background-color', '#a4161a');
+                $("#odg2").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next2, 3000);
+            })
+
+            $("#odg4").click(function(){
+                $("#odg4").css('background-color', '#a4161a');
+                $("#odg2").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next2, 3000);
+            })
+
+            $("#next").click(function(){
+                $("#odg2").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next2, 3000);
+            })
 
         function next2(){
+            $(".btn").css('background-color', 'black');
+            $(".btn").attr("disabled", false);
             $("#text").html(data.drugo_pitanje);
             $("#odg1").html(data.odgovori_pitanja2[0]);
             $("#odg2").html(data.odgovori_pitanja2[1]);
             $("#odg3").html(data.odgovori_pitanja2[2]);
-            $("#odg4").html(data.odgovori_pitanja1[3]);
-            setTimeout(next3, 3000);
+            $("#odg4").html(data.odgovori_pitanja2[3]);
+
+            $("#odg1").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg1").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next3, 3000);
+            })
+
+            $("#odg2").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg2").css('background-color', '#a4161a');
+                $("#odg1").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next3, 3000);
+            })
+
+            $("#odg3").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg3").css('background-color', '#a4161a');
+                $("#odg1").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next3, 3000);
+            })
+
+            $("#odg4").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg4").css('background-color', '#a4161a');
+                $("#odg1").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next3, 3000);
+            })
+
+            $("#next").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg1").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next3, 3000);
+            })
         }
 
         function next3(){
+            $(".btn").css('background-color', 'black');
+            $(".btn").attr("disabled", false);
             $("#text").html(data.trece_pitanje);
             $("#odg1").html(data.odgovori_pitanja3[0]);
             $("#odg2").html(data.odgovori_pitanja3[1]);
             $("#odg3").html(data.odgovori_pitanja3[2]);
             $("#odg4").html(data.odgovori_pitanja3[3]);
-            setTimeout(next4, 3000);
+
+            $("#odg1").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg1").css('background-color', '#a4161a');
+                $("#odg3").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next4, 3000);
+            })
+
+            $("#odg2").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg2").css('background-color', '#a4161a');
+                $("#odg3").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next4, 3000);
+            })
+            
+            $("#odg3").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg3").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next4, 3000);
+            })
+
+            $("#odg4").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg4").css('background-color', '#a4161a');
+                $("#odg3").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next4, 3000);
+            })
+
+            $("#next").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg3").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next4, 3000);
+            })
         }
 
         function next4(){
+            $(".btn").css('background-color', 'black');
+            $(".btn").attr("disabled", false);
             $("#text").html(data.cetvrto_pitanje);
             $("#odg1").html(data.odgovori_pitanja4[0]);
             $("#odg2").html(data.odgovori_pitanja4[1]);
             $("#odg3").html(data.odgovori_pitanja4[2]);
             $("#odg4").html(data.odgovori_pitanja4[3]);
-            setTimeout(next5, 3000);
+
+            $("#odg1").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg1").css('background-color', '#a4161a');
+                $("#odg3").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next5, 3000);
+            })
+
+            $("#odg2").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg2").css('background-color', '#a4161a');
+                $("#odg3").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next5, 3000);
+            })
+            
+            $("#odg3").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg3").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next5, 3000);
+            })
+
+            $("#odg4").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg4").css('background-color', '#a4161a');
+                $("#odg3").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next5, 3000);
+            })
+
+            $("#next").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg3").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next5, 3000);
+            })
         }
 
         function next5(){
+            $(".btn").css('background-color', 'black');
+            $(".btn").attr("disabled", false);
             $("#text").html(data.peto_pitanje);
             $("#odg1").html(data.odgovori_pitanja5[0]);
             $("#odg2").html(data.odgovori_pitanja5[1]);
             $("#odg3").html(data.odgovori_pitanja5[2]);
             $("#odg4").html(data.odgovori_pitanja5[3]);
-            setTimeout(next6, 3000);
+            
+            $("#odg1").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg1").css('background-color', '#a4161a');
+                $("#odg2").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next6, 3000);
+            })
+            
+            $("#odg2").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg2").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next6, 3000);
+            })
+
+            $("#odg3").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg3").css('background-color', '#a4161a');
+                $("#odg2").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next6, 3000);
+            })
+
+            $("#odg4").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg4").css('background-color', '#a4161a');
+                $("#odg2").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next6, 3000);
+            })
+
+            $("#next").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg2").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next6, 3000);
+            })
         }
         function next6(){
+            $(".btn").css('background-color', 'black');
+            $(".btn").attr("disabled", false);
             $("#text").html(data.sesto_pitanje);
             $("#odg1").html(data.odgovori_pitanja6[0]);
             $("#odg2").html(data.odgovori_pitanja6[1]);
             $("#odg3").html(data.odgovori_pitanja6[2]);
             $("#odg4").html(data.odgovori_pitanja6[3]);
-            setTimeout(next7, 3000);
+            
+            $("#odg1").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg1").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next7, 3000);
+            })
+
+            $("#odg2").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg2").css('background-color', '#a4161a');
+                $("#odg1").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next7, 3000);
+            })
+
+            $("#odg3").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg3").css('background-color', '#a4161a');
+                $("#odg1").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next7, 3000);
+            })
+
+            $("#odg4").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg4").css('background-color', '#a4161a');
+                $("#odg1").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next7, 3000);
+            })
+
+            $("#next").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg1").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+                setTimeout(next7, 3000);
+            })
         }
         function next7(){
+            $(".btn").css('background-color', 'black');
+            $(".btn").attr("disabled", false);
             $("#text").html(data.sedmo_pitanje);
             $("#odg1").html(data.odgovori_pitanja7[0]);
             $("#odg2").html(data.odgovori_pitanja7[1]);
             $("#odg3").html(data.odgovori_pitanja7[2]);
             $("#odg4").html(data.odgovori_pitanja7[3]);
+
+            $("#odg1").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg1").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+            })
+
+            $("#odg2").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg2").css('background-color', '#a4161a');
+                $("#odg1").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+            })
+
+            $("#odg3").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg3").css('background-color', '#a4161a');
+                $("#odg1").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+            })
+
+            $("#odg4").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg4").css('background-color', '#a4161a');
+                $("#odg1").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+            })
+
+            $("#next").click(function(){
+                $(".btn").css('background-color', 'black');
+                $("#odg1").css('background-color', '#49a078');
+                $(".btn").attr("disabled", "disabled");
+            })
         }
     }
     )
