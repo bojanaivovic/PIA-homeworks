5f85de604cafaee189943cea5426c54a60c4a7d3
bojanaivovic <bbojana.ivovic@gmail.com>
2020-12-25 13:51:28 +0100

    dodata pitanja za unos sa tastature

--- hw-3/quiz.js
+++ hw-3/quiz.js
@@ -42,12 +42,24 @@ function start(){
 function pokaziPitanje(){
   $(".btn").css('background-color', 'black');
   $(".btn").attr("disabled", false);
-  document.getElementById("text").innerHTML = pitanja[brojac].pitanje;
-  var odg = randomBrojevi();
-  document.getElementById("odg1").innerHTML = pitanja[brojac].odgovori[odg[0]];
-  document.getElementById("odg2").innerHTML = pitanja[brojac].odgovori[odg[1]];
-  document.getElementById("odg3").innerHTML = pitanja[brojac].odgovori[odg[2]];
-  document.getElementById("odg4").innerHTML = pitanja[brojac].odgovori[odg[3]];
+  if(brojac%2==0){
+      $('.odgovori_4').show();
+      $('.odgovori_dopuna').hide();
+      document.getElementById("text").innerHTML = pitanja[brojac].pitanje;
+      var odg = randomBrojevi();
+      document.getElementById("odg1").innerHTML = pitanja[brojac].odgovori[odg[0]];
+      document.getElementById("odg2").innerHTML = pitanja[brojac].odgovori[odg[1]];
+      document.getElementById("odg3").innerHTML = pitanja[brojac].odgovori[odg[2]];
+      document.getElementById("odg4").innerHTML = pitanja[brojac].odgovori[odg[3]];
+  }
+  else{
+      $('#prikaz_odg').hide();
+      $('.odgovori_4').hide();
+      $('.odgovori_dopuna').show();
+      document.getElementById("tekstualni_odgovor").style.color = 'white';
+      document.getElementById("tekstualni_odgovor").value="";
+      document.getElementById("text").innerHTML = pitanja[brojac].pitanje;
+  }
 }
 
 function randomBrojevi(){
@@ -57,17 +69,22 @@ function randomBrojevi(){
 }
 
 function tacanOdgovor(btn){
-  if(btn.innerHTML==pitanja[brojac].odgovori[1]){
-    btn.style.backgroundColor = "#49a078";
-    $(".btn").attr("disabled", "disabled"); 
-    brojac+=1;
-    dodajPoen();
-    setTimeout(pokaziPitanje, 3000);                                 
+  if(brojac%2==0){
+      if(btn.innerHTML==pitanja[brojac].odgovori[1]){
+        btn.style.backgroundColor = "#49a078";
+        $(".btn").attr("disabled", "disabled"); 
+        brojac+=1;
+        dodajPoen();
+        setTimeout(pokaziPitanje, 3000);                                 
+      }
+      else{
+        btn.style.backgroundColor = "#a4161a";
+        nadjiOdgovor();
+      }
   }
   else{
-    btn.style.backgroundColor = "#a4161a";
-    nadjiOdgovor();
-  }
+    dopunaTacnost();
+    }
 }
 
 function nadjiOdgovor(){
@@ -96,13 +113,31 @@ function nadjiOdgovor(){
     setTimeout(pokaziPitanje, 3000); 
   }
 }
+  
+function datOdgovor(){
+    var odg=document.getElementById("tekstualni_odgovor").value.toUpperCase(); 
+    $('#prikaz_odg').show();
+    if((pitanja[brojac].odgovori)==odg.trim()){
+      document.getElementById("prikaz_odg").innerHTML="Tačno!";
+      $("#tekstualni_odgovor").css('color', '#49a078');
+      dodajPoen();
+      brojac+=1;
+      setTimeout(pokaziPitanje, 3000); 
+    }
+    else{
+      document.getElementById("prikaz_odg").innerHTML=pitanja[brojac].odgovori;
+      brojac+=1;
+      setTimeout(pokaziPitanje, 3000); 
+    }
+}
+
+function sledecePitanje(){
+    brojac+=1;
+    pokaziPitanje();
+}
 
 function dodajPoen(){
   poeni+=1;
   document.getElementById("poeni").innerHTML = poeni; 
 }
 
-$("#next").click(function(){
-  if(brojac<7)
-    nadjiOdgovor();
-})
